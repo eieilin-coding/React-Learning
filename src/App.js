4b09b979806989210bfb22d0b99af4ebd5995be9
Ei Ei Lin <Eieilin@uab.com.mm>
2026-03-29 16:11:23 +0630

    show / hide modal box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/index';
+import PostsList from './components/PostsList/index';
+import Modal from './components/Modal/index';
 import {useState} from 'react';
 
 function App() {
 
-
+let [showModal, setShowModal] = useState(false);
 let [posts, setPosts] = useState([
   {
     id : 1,
@@ -23,15 +26,19 @@ let [posts, setPosts] = useState([
   
 
   return (
-    <nav>
-      <div className='container'>
-        <h1>Logo</h1>
-        <ul>
-          <li>Home</li>
-          <li>Posts</li>
-        </ul>
-      </div>
-    </nav>    
+    <>
+    <Navbar setShowModal={setShowModal}/>
+    <PostsList posts={posts}/>
+    {/* <Modal>
+      <h1>Zoom class is available now.</h1>
+            <p>Feel free to <a href="">join</a> here</p>
+    </Modal> */}
+    {showModal && <Modal danger>
+      <h1>Terms and Conditions</h1>
+      <p>Lorem ipsum dolor sit amdet lakid. Feace the fear! You will win.</p>
+      <button onClick={() => setShowModal(false)}>close</button>
+    </Modal>}
+    </>
   );
 }
 
