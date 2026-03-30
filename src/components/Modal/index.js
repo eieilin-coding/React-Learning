f50c15d328c055e55bc064d3a9ae8aabf77ed4de
Ei Ei Lin <Eieilin@uab.com.mm>
2026-03-30 09:03:34 +0630

    props multi component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,12 +3,12 @@ import './index.css';
 import ReactDom from 'react-dom';
 
 export default function Modal({children, danger=false}) {
+        let className = danger ? 'border-red' : 'border-blue';
   return (
     ReactDom.createPortal(
         <div className='modal-component'>
             <div className='modal-backdrop'>
-                <div className="modal" style={{ border : '4px solid',
-                     borderColor : danger ? 'red' : 'blue'}}>
+                <div className={`modal ${className}`} >
                     {children}
                 </div>
             </div>
